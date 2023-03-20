a9178e50f9f2c6bf35b2207d9888dd23092e0b66
Kaia Scheirman <kfscheirman18@gmail.com>
2023-03-20 13:37:40 -0700

    pull elements into js using DOM

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// forms
+const startForm = document.getElementById("start-form");
+const secondForm = document.getElementById("second-form");
+const thirdForm = document.getElementById("third-form");
+const fourthForm = document.getElementById("fourth-form");
+const fifthForm = document.getElementById("fifth-form");
+const endForm = document.getElementById("end-form");
+
+// end and bottom pages
+const endPage = document.getElementById("end-page");
+const bottomPage = document.getElementById("bottom-page");
+
+// form buttons
+const startFormBtn = document.getElementById("get-started");
+const secondFormBtn = document.getElementById("make-next");
+const thirdFormBtn = document.getElementById("mileage-next");
+const fourthFormBtn = document.getElementById("zipcode-next");
+const fifthFormBtn = document.getElementById("name-next");
+const endFormBtn = document.getElementById("get-quote");
